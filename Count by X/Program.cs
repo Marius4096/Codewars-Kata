5e063400558dac91b3fc0afdece122ab5c3c68cf
Marius <Marius@DESKTOP-NVP7LM1>
2022-08-01 06:59:53 +0100

    Add project files.

---
+++ Count by X/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Count_by_X
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine(string.Join("," ,CountBy(1, 5)));
+            Console.WriteLine(string.Join(",", CountBy(2, 5)));
+        }
+        public static int[] CountBy(int x, int n)
+        {
+            int x1 = x;
+            int[] arr = new int[n];
+            for(int i = 0; i < n; i++)
+            {
+                arr[i] += x1;
+                x1 += x;
+            }
+            return arr;
+        }
+    }
+}
+/*
+ 
+ public static int[] CountBy(int x, int n)
+  {
+    return Enumerable.Range(1, n).Select(i => i * x).ToArray();    
+  }
+ 
+public static int[] CountBy(int x, int n)
+  {
+    return Enumerable.Range(1, n).Select(e => e * x).ToArray();
+  }
+
+ */
